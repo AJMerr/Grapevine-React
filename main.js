96269f00d0c799e36de9fe6f2a28ec5c8f3d6c68
Austin <austinm9506@gmail.com>
2021-08-02 02:22:28 -0400

    Added a webserver as well as a script in the package JSON to run the server easier

---
+++ main.js
@@ -0,0 +1,35 @@
+// Imports for packages
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+
+
+// Middleware registration
+// Parses through the body of the HTTP requests from a URL encoded string 
+app.use(express.urlencoded({extended: true}))
+
+
+ // Parses through the body of the HTTP requests from a JSON string  
+app.use(express.json())
+
+
+ //add method-override middleware for "faking" DELETE and PUT/PATCH requests 
+app.use(methodOverride('_method'))
+
+
+//Creates routers for the application, first arguement is the path
+//app.use('/api/', filler)
+
+
+ //Uses the client/build directory to host CSS and images
+ app.use(express.static(`${__dirname}/client/build`))
+
+
+ // Set the port the server is to run on
+const PORT = process.env.PORT || 8080 
+
+
+//Starts the server
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`)
+})
