6f8e1f4f543ddc19c99e15e0fbb4e58a8e26eafd
Austin <austinm9506@gmail.com>
2021-08-03 21:46:10 -0400

    Added a page for creating new seeds (messages) that will then display on the main page

--- grapevine-react/src/App.js
+++ grapevine-react/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AllSeeds from './components/AllSeeds';
+import CreateSeed from './components/CreateSeed';
 import './App.css';
 require ("./components/styles.css")
 
@@ -11,6 +12,7 @@ class App extends React.Component {
         <div>
           <Switch>
             <Route exact path = "/"component = {AllSeeds} />
+            <Route exact path= "/newseed" component = {CreateSeed} />
           </Switch>
         </div>
       </Router>
---
+++ grapevine-react/src/components/CreateSeed.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import axios  from 'axios'
+
+export default class CreateSeed extends Component {
+
+    state = {
+        newSeed: {
+            seed: '',
+            created_at: ''
+        }
+    }
+
+    chaangeHandler = (e) => {
+        const copyNewSeed = { ...this.state.newSeed }
+        copyNewSeed[e.target.name] = e.target.value
+        this.setState({ newSeed: copyNewSeed })
+    }
+
+    newSeed = (e) => {
+        e.preventDefault()
+        // eslint-disable-next-line no-lone-blocks
+        {
+            axios.post("api/grapevine", {
+                seed: this.state.newSeed.seed,
+                created_at: this.state.newSeed.created_at
+            })
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.newSeed}>
+                    <label>
+                        New Grape: 
+                        <input id="seed" type="text" value={this.state.newSeed.seed} onChange={this.chaangeHandler} name="seed"/>
+                    </label>
+                    <label>
+                        Created At:
+                        <input id="created at" type="text" value={this.state.newSeed.created_at} onChange={this.chaangeHandler} name="created at"/>
+                    </label>
+                    <button onClick={this.newSeed}> Sumbit </button>
+                </form>
+            </div>
+        )
+    }
+}
