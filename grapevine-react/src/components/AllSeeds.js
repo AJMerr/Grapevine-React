1c8e4a85e0977a91b254cdf88188ebb04b9e370d
Austin <austinm9506@gmail.com>
2021-08-05 02:29:46 -0400

    Removed the DELETE function and button

--- grapevine-react/src/components/AllSeeds.js
+++ grapevine-react/src/components/AllSeeds.js
@@ -25,25 +25,13 @@ export default class AllSeeds extends Component {
         }
     }
 
-    onDelete = async () => {
-        try {
-            const res = await axios.delete(`api/grapevine/${this.props.match.params.id}`)
-            this.setState({ allSeeds: res.data })
-        }
-        catch (err) {
-            console.log(err)
-            this.setState({ error: err.message })
-        }
-    }
-
     render() {
         const plantedSeed = this.state.allSeeds.map((seeds, i) =>{
             return(
                 <ul key={seeds._id} >
-                <li className="messages">
-                    {seeds.seed}
-                </li>
-                <button onClick={this.onDelete.bind(this)}>Delete</button>
+                    <li className="messages">
+                        {seeds.seed}
+                    </li>
                 </ul>
             )
         })
